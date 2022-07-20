7d5c4ef073c16ff9bd80baf5f6a6bfb87d42e613
mulori <murilogarcia.hr@gmail.com>
2022-07-19 23:58:46 -0300

    tela de explorar

--- src/screen/mainActivity/index.js
+++ src/screen/mainActivity/index.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, TouchableOpacity, Text, Modal, StyleSheet, StatusBar, FlatList, Image, RefreshControl, ScrollView } from 'react-native';
 import mainservices from '../../services/mainService/mainService';
 import activityServices from '../../services/activityService/activityService';
 import WindMill from '../../components/lotties/WindMill';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import * as Animatable from 'react-native-animatable';
 import VG from '../../components/variables/VG';
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
@@ -17,6 +18,7 @@ export default function MainActivity({ navigation }) {
     const [configMenu, setConfigMenu] = useState(null);
     const [listActivitys, setListActivitys] = useState(null);
     const [refreshing, setRefreshing] = React.useState(false);
+    const [bannerVisible, setBannerVisible] = useState(true);
 
     const encodedData = 'R0lGODlhAQABAIAAAAAA...7';
 
@@ -31,115 +33,145 @@ export default function MainActivity({ navigation }) {
         GetActvity();
     }, [])
 
-    function setModalMenu(value){
+    function setModalMenu(value) {
         setModalVisible(value);
     }
 
-    function configMenuMain(){
+    function configMenuMain() {
         mainservices.GetConfigMenu(VG.user_uid)
-        .then((response) => {
-            setConfigMenu(response.data);      
-        })
-        .catch((error) => {
-            console.log(error);
-        })         
+            .then((response) => {
+                setConfigMenu(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
-    function GetActvity(){
+    function GetActvity() {
         activityServices.GetActivitys(VG.user_uid)
-        .then((response) => {
-            setListActivitys(response.data);    
-        })
-        .catch((error) => {
-            console.log(error);
-        })         
+            .then((response) => {
+                setListActivitys(response.data);
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
-    function CaseScreen(item){
-        switch(item) { 
-            case 1:             
+    function CaseScreen(item) {
+        switch (item) {
+            case 1:
                 navigation.navigate('newActivity')
                 setModalVisible(false)
                 break;
-            case 2:             
+            case 2:
                 navigation.navigate('myActivity')
                 setModalVisible(false)
                 break;
-            case 3:             
+            case 3:
                 navigation.navigate('searchActivity')
                 setModalVisible(false)
                 break;
-            case 4:             
+            case 4:
                 navigation.navigate('myProfile')
                 setModalVisible(false)
                 break;
             case 5:
                 auth().signOut();
-                break;          
+                break;
         }
     }
 
-    function Enter(item){
-        if(item.author_uid == VG.user_uid){
-            navigation.navigate('usersActivity', { activity: item})
-        }else{
-            if(item.with_password == 1){
-                navigation.navigate('passActivity', { activity: item})
-            }else{
-                navigation.navigate('mainSearchActivity', { activity: item})
+    function Enter(item) {
+        if (item.author_uid == VG.user_uid) {
+            navigation.navigate('usersActivity', { activity: item })
+        } else {
+            if (item.with_password == 1) {
+                navigation.navigate('passActivity', { activity: item })
+            } else {
+                navigation.navigate('mainSearchActivity', { activity: item })
             }
         }
-    }   
+    }
 
-    return(
+    return (
         <View style={style.container}>
-            <StatusBar barStyle='dark-content' backgroundColor='#FFF' />      
-
-            <ScrollView 
-            refreshControl={
-                <RefreshControl
-                refreshing={refreshing}
-                onRefresh={onRefresh}
-                />
-            }
-            >
-               {!listActivitys ? null : 
-                    listActivitys.map((item, key) => 
-                    <TouchableOpacity key={key} onPress={() => {Enter(item)}} style={{ 
-                        backgroundColor: '#365663',                     
-                        margin: 5,
-                        borderRadius: 15,                
-                        }}>
-                        <View style={{flexDirection: 'row'}}>
+            <StatusBar barStyle='dark-content' backgroundColor='#FFF' />
+            {
+                !bannerVisible ? null :
+                    <View style={{ width: '90%', height: '18%', }}>
                         <Image
-                            style={style.logo}
-                            source={{
-                            uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-                            }}
+                            source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/eduwi-64db3.appspot.com/o/banner%2Finternos%2Fbanner_explore.png?alt=media&token=a844fe1d-9ac4-4edd-a167-3f340f316682' }}
+                            style={{ width: '100%', height: '100%', borderRadius: 20 }}
                         />
-                            {/* <View style={{ backgroundColor: 'rgba(0,0,0,0.4)', width:'86%', borderBottomStartRadius: 15, borderTopLeftRadius: 15, padding: 15}}>
-                                <Text style={{color: '#FFF', fontSize: 15, fontWeight: 'bold', borderColor: '#000', fontStyle: 'italic' }}>{item.title}</Text>     
-                            </View> */}
+                        <TouchableOpacity
+                            onPress={() => setBannerVisible(false)}
+                            style={{ position: 'absolute', right: 20, top: 5, backgroundColor: '#FFF', padding: 2, borderRadius: 25 }}>
+                            <FontAwesome name='close' size={25} style={{ color: '#000' }} />
+                        </TouchableOpacity>
+                    </View>
+            }
+            <View style={{ width: '100%', marginTop: 25}}>
+            <Text style={{ left: 20, fontSize: 20, fontWeight: 'bold' }}>Atividades Populares</Text>
+            </View>
+            <ScrollView
+                style={{ width: '100%', }}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
+            >
+                {!listActivitys ? null :
+                    listActivitys.map((item, key) =>
+                        <View
+                            style={{
+                                width: '100%',
+                                alignItems: 'center',
+                                height: '100%',
+                            }}>
+                            <TouchableOpacity key={key} onPress={() => { Enter(item) }} style={{
+                                backgroundColor: '#365663',
+                                margin: 5,
+                                height: 150,
+                                borderRadius: 15,
+                                padding: 20,
+                                width: '90%'
+                            }}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Image
+                                        style={style.logo}
+                                        source={item.image_url ? { uri: item.image_url } : require('../../assets/image/imageNotFound.png')}
+                                    />
+                                    <View>
+                                        <Text style={{color: '#FFF', fontSize: 15, fontWeight: 'bold', borderColor: '#000', fontStyle: 'italic', left: 5 }}>{item.title}</Text>     
+                                    </View>
+                                    <Text style={{ color: '#FFF', position: 'absolute', bottom: 0, left: 115, fontSize: 15 }}>Dificuldade: Média</Text>
+                                    <FontAwesome name='star' size={22} style={{ color: '#FFF', position: 'absolute', bottom: 3, right: 20}} />
+                                    <Text style={{ color: '#FFF', position: 'absolute', bottom: 0, right: 5, fontSize: 22 }}>5</Text>
+                                </View>
+
+
+                            </TouchableOpacity>
                         </View>
-                        
-                        
-                    </TouchableOpacity>             
-               )}
-            </ScrollView>         
+
+                    )}
+            </ScrollView>
 
             <Modal visible={modalVisible} animationType="slide" >
-                <View style={{backgroundColor: '#FFF', padding: 12, flexDirection: 'row'}}>
-                    <TouchableOpacity onPress={() => {setModalMenu(false)}}>
-                        <Icon name="arrow-left" size={26} style={{ color : '#000000'}} />
-                    </TouchableOpacity>                    
-                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000000', marginLeft: 10}}>Voltar</Text>
+                <View style={{ backgroundColor: '#FFF', padding: 12, flexDirection: 'row' }}>
+                    <TouchableOpacity onPress={() => { setModalMenu(false) }}>
+                        <Icon name="arrow-left" size={26} style={{ color: '#000000' }} />
+                    </TouchableOpacity>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000000', marginLeft: 10 }}>Voltar</Text>
                 </View>
 
                 <FlatList data={configMenu} keyExtractor={item => item.id} renderItem={({ item }) => {
                     return (
-                        <TouchableOpacity key={item.id} onPress={() => CaseScreen(item.id)} style={{ backgroundColor: item.color_background, flexDirection: 'row', padding: 15, borderRadius: 20, margin: 5}}>
-                            <Icon name={item.name_icon} style={{ color: item.color_icon, marginRight: 10}} size={20} />
-                            <Text style={{ color: item.color_title}}>{item.title}</Text>
+                        <TouchableOpacity key={item.id} onPress={() => CaseScreen(item.id)} style={{ backgroundColor: item.color_background, flexDirection: 'row', padding: 15, borderRadius: 20, margin: 5 }}>
+                            <Icon name={item.name_icon} style={{ color: item.color_icon, marginRight: 10 }} size={20} />
+                            <Text style={{ color: item.color_title }}>{item.title}</Text>
                         </TouchableOpacity>
                     );
                 }}
@@ -153,7 +185,8 @@ export default function MainActivity({ navigation }) {
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFF'
+        backgroundColor: '#FFF',
+        alignItems: 'center',
     },
     searchCommunity: {
         position: 'absolute',
@@ -168,7 +201,8 @@ const style = StyleSheet.create({
         elevation: 10
     },
     logo: {
-        height: 50,
-        width: 50,
+        height: 110,
+        width: 110,
+        borderRadius: 25,
     }
 })
